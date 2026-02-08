e8f30518a841a4c0e958a7b447363a99681f488d
Shockwave <markiewiczmichal160390@gmail.com>
2026-02-08 12:48:24 +0100

    Initial commit

---
+++ backend/server.js
@@ -0,0 +1,38 @@
+//1. Importy
+const express = require('express');
+const { Pool } = require('pg');
+
+//2. Tworzymy aplikację express (wynik funkcji express to obiekt z wbudowanymi metodami własnymi)
+const app = express();
+
+//3. Middleware do obsługi json
+app.use(express.json());
+
+//4. Połączenie z bazą danych PostgreSQL
+const pool = new Pool({
+    user: 'postgres',
+    host: 'localhost',
+    database: 'candleshop',
+    password: 'Kikinowy16031990',
+    port: 5432
+});
+
+//5. Endpoint testowy
+app.get('/', (req, res) => {
+    res.send('backend działa!');
+});
+
+//6. Endpoint z bazy danych
+app.get('/produkty', async (req, res) => {
+    try {
+        const result = await pool.query('SELECT * FROM products');
+        res.json(result.rows);
+    } catch (err) {
+        console.error(err);
+        res.status(500).send('błąd serwera');
+    }
+});
+
+app.listen(3000, () => {
+    console.log('Server działa na http://localhost:3000');
+});
